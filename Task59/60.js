a76a2e74ff09c173c0bb6a3dd94e11acfc9a2d13
Lus2017 <Sirekanyan.91@mail.ru>
2017-03-12 00:23:24 +0400

    Task 59/60

---
+++ Task59/60.js
@@ -0,0 +1,44 @@
+function checkNumbers(number){
+    const num=number;
+    const a=(num-num%100)/100;
+    const b=((num-a*100)-num%10)/10;
+    const c=num%10;
+    var max , middle, min;
+
+    if(a>b&&a>c){
+        max=a;
+        if(b>c){
+            middle=b;
+            min=c;
+        }else{
+            middle=c;
+            min=b;
+        }
+    }
+
+    else if(b>a&&b>c){
+        max=b;
+
+        if(a>c){
+            middle=a;
+            min=c;
+        }else{
+            middle=c;
+            min=a;
+        }
+    }
+
+    else{
+        max=c;
+        if(b>a){
+            middle=b;
+            min=a;
+        }else{
+            middle=a;
+            min=b;
+        }
+    }
+    alert(max+' '+middle+' '+min);
+    alert(min+' '+middle+' '+max);
+}
+checkNumbers(567);
