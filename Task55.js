58a49461f8268cf4256ce4a344cf3f5e04724928
Lus2017 <Sirekanyan.91@mail.ru>
2017-03-11 22:58:01 +0400

    Task 55

---
+++ Task55.js
@@ -0,0 +1,16 @@
+function checkNumbers(number){
+//
+    const num=number;
+    const a=(num-num%100)/100;
+    const b=((num-a*100)-num%10)/10;
+    const c=num%10;
+    if (a<b && a<c) {
+        alert(a);
+    }
+    else if(b<a && b<c){
+        alert(b);
+    }
+    else{
+        alert(c);
+    }
+}
