3660197d2f7363d1fa22842e07e3632291e9e2e0
Lus2017 <Sirekanyan.91@mail.ru>
2017-03-12 23:24:53 +0400

    Task 62

---
+++ Task62.js
@@ -0,0 +1,16 @@
+function checkNumbers(number){
+    const num=number;
+    var t=true;
+    const a=(num-num%1000)/1000;
+    const b=((num -a*1000)-num%100)/100;
+    debugger;
+    const c=(num-a*1000-b*100-num%10)/10;
+    const d=num%10;
+    if (num<5000) {
+        alert(num/(d+b));
+    }
+    else{
+        alert(num/(a+c));
+    }
+}
+checkNumbers(2222);
