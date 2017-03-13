fc6b17fe50ee297daac585a805f9252d2f4b29f8
Lus2017 <Sirekanyan.91@mail.ru>
2017-03-13 23:51:35 +0400

    Task 66

---
+++ Task66.js
@@ -0,0 +1,14 @@
+function checkNumbers(number){
+    const num=number;
+    const a=(num-num%1000)/1000;
+    const b=((num -a*1000)-num%100)/100;
+    const c=(num-a*1000-b*100-num%10)/10;
+    const d=num%10;
+    if (a==4 || d==4) {
+        alert('YES');
+    }
+    else{
+        alert('NO');
+    }
+}
+checkNumbers(5893);
